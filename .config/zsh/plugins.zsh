1426bbfbcfff1f2a31ed7ef4290a2617f5b32848
Illia Bilyk <illia.krasn0glaz@gmail.com>
2024-10-07 10:55:33 +0200

    Updated atuin configuration

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -6,6 +6,7 @@ fi
 
 if ! [ -d ~/.atuin/bin ]; then
 		curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+		source $HOME/.atuin/bin/env
 fi
 
 eval "$(~/.atuin/bin/atuin init zsh --disable-up-arrow)"
